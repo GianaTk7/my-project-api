cf35f8008aea8be4a8d53abbce0a10c68c6d8c39
GianaTk7 <tkgenia1234@gmail.com>
2024-10-14 21:26:17 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -99,6 +99,21 @@ app.get('/Stores/:storeName', async (req, res) => {
     res.status(500).json({ message: error.message || "Internal Server Error" });
   }
 });
+app.get('/users', async (req, res) => {
+  try {
+    const email = req.params.email; 
+    const collection = db.collection("User"); 
+    const user = await collection.find({email:email})
+
+    if (user.length < 1) {
+      return res.status(404).json({ message: "User not found" });
+    }
+     return res.status(200).json(user)
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ message: error.message || "Internal Server Error" });
+  }
+});
 
 // Update user details
 app.put('/users/:email', async (req, res) => {
