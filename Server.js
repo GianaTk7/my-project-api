6826fa02f5904d4f94ebfecc92b4c4094f55b40e
GianaTk7 <tkgenia1234@gmail.com>
2024-10-14 21:33:35 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -102,7 +102,7 @@ app.get('/Stores/:storeName', async (req, res) => {
 app.get('/users', async (req, res) => {
   try {
     const collection = db.collection("User"); 
-    const user = await collection.find({}).toArray{}
+    const user = await collection.find({}).toArray()
 
     if (user.length < 1) {
       return res.status(404).json({ message: "User not found" });
