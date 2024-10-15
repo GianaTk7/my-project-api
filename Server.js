61c1736f3efe6ea30c980b6b9925b98b2d7d226e
GianaTk7 <tkgenia1234@gmail.com>
2024-10-15 18:31:19 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -80,15 +80,22 @@ app.post("/signUp", async (req, res) => {
   }
 });
 app.get('/Stores', async (req, res) => {
-    try {
-        console.log("Fetching stores");
-        const stores = await db.collection('Stores').find({}).toArray();
-        console.log(stores); // Log the result to check if it's fetching data
-        res.json(stores);
-    } catch (error) {
-        console.error("Error retrieving Stores: ", error); // Log any errors
-        res.status(500).send("Error retrieving Stores");
-    }
+  try {
+      console.log("Fetching stores by address");
+      const address = req.query.address; 
+      const stores = await db.collection('Stores').find({ Address: address }).toArray();
+      
+      console.log(stores); 
+      
+      if (stores.length === 0) {
+          res.status(404).send("No stores found at this address");
+      } else {
+          res.status(200).json(stores);
+      }
+  } catch (error) {
+      console.error("Error retrieving Stores: ", error);
+      res.status(500).send("Error retrieving Stores");
+  }
 });
 
 // GET user by email
